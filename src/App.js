d50d005f083f6a7ffcd7626a47762b4a69a6eb9d
Pegah Eizadkhah <peizadkh@gmail.com>
2019-06-04 09:38:33 -0400

    changes to use state

--- src/App.js
+++ src/App.js
@@ -1,76 +1,109 @@
-import React, { useState, Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 import AddUserForm from './forms/AddUserForm'
 import EditUserForm from './forms/EditUserForm'
 import UserTable from './tables/UserTable'
 
-const App = () => {
+class App extends Component {
 	// Data
-	const usersData = [
+    usersData = [
 		{ id: 1, name: 'Tania', username: 'floppydiskette' },
 		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
 		{ id: 3, name: 'Ben', username: 'benisphere' },
 	]
 
-	const initialFormState = { id: null, name: '', username: '' }
+    initialFormState = { id: null, name: '', username: '' }
 
 	// Setting state
-	const [ users, setUsers ] = useState(usersData)
-	const [ currentUser, setCurrentUser ] = useState(initialFormState)
-	const [ editing, setEditing ] = useState(false)
+
+	// const [ users, setUsers ] = useState(usersData)
+	// const [ currentUser, setCurrentUser ] = useState(initialFormState)
+	// const [ editing, setEditing ] = useState(false)
+
+	state = {
+		users: [
+			...this.usersData
+		],
+		currentUser: this.initialFormState,
+		editing: false
+	}
 
 	// CRUD operations
-	const addUser = user => {
-		user.id = users.length + 1
-		setUsers([ ...users, user ])
+	addUser = user => {
+		user.id = this.state.users.length + 1
+		// setUsers([ ...users, user ])
+		this.setState(() => {
+			return {
+			  users: [...this.usersData, user]
+			}
+		  });
 	}
 
-	const deleteUser = id => {
-		setEditing(false)
+	deleteUser = id => {
+		// setEditing(false)
 
-		setUsers(users.filter(user => user.id !== id))
+		// setUsers(users.filter(user => user.id !== id))
+        this.setState(() => {
+			return {
+				editing: false,
+				users: this.state.users.filter(user => user.id !== id)
+			}
+		});
 	}
 
-	const updateUser = (id, updatedUser) => {
-		setEditing(false)
+	updateUser = (id, updatedUser) => {
+		// setEditing(false)
 
-		setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+		// setUsers(users.map(user => (user.id === id ? updatedUser : user)))
+		this.setState(() => {
+			return {
+				editing: false,
+				users: this.state.users.map(user => (user.id === id ? updatedUser : user))
+			}
+		});
 	}
 
-	const editRow = user => {
-		setEditing(true)
+	editRow = user => {
+		// setEditing(true)
 
-		setCurrentUser({ id: user.id, name: user.name, username: user.username })
+		// setCurrentUser({ id: user.id, name: user.name, username: user.username })
+		this.setState(() => {
+			return {
+				editing: true,
+				currentUser: { id: user.id, name: user.name, username: user.username }
+			}
+		});
 	}
-
-	return (
-		<div className="container">
-			<h1>CRUD App with Hooks</h1>
-			<div className="flex-row">
-				<div className="flex-large">
-					{editing ? (
-						<Fragment>
-							<h2>Edit user</h2>
-							<EditUserForm
-								editing={editing}
-								setEditing={setEditing}
-								currentUser={currentUser}
-								updateUser={updateUser}
-							/>
-						</Fragment>
-					) : (
-						<Fragment>
-							<h2>Add user</h2>
-							<AddUserForm addUser={addUser} />
-						</Fragment>
-					)}
-				</div>
-				<div className="flex-large">
-					<h2>View users</h2>
-					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+	
+	render() {
+		return (
+			<div className="container">
+				<h1>CRUD App with Hooks</h1>
+				<div className="flex-row">
+					<div className="flex-large">
+						{this.state.editing ? (
+							<Fragment>
+								<h2>Edit user</h2>
+								<EditUserForm
+									editing={this.state.editing}
+									currentUser={this.state.currentUser}
+									updateUser={this.updateUser}
+								/>
+							</Fragment>
+						) : (
+							<Fragment>
+								<h2>Add user</h2>
+								<AddUserForm addUser={this.addUser} />
+							</Fragment>
+						)}
+					</div>
+					<div className="flex-large">
+						<h2>View users</h2>
+						<UserTable users={this.usersData} editRow={this.editRow} deleteUser={this.deleteUser} />
+					</div>
 				</div>
 			</div>
-		</div>
-	)
+		)
+	}
 }
 
 export default App
